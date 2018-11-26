024c28d3d7ef531d0785ebb505bbdc71bc2bd2cb
Benzil <benzil.john49@gmail.com>
2018-11-26 09:50:51 -0500

    Add files via upload

---
+++ 2. Process Scheduling/h/math.h
@@ -0,0 +1,12 @@
+#ifndef _math_h_
+#define _math_h_
+
+#define RAND_MAX 077777
+
+extern double pow(double x,double y);
+extern double log(double x);
+extern double expdev(double lambda);
+
+
+#endif
+
